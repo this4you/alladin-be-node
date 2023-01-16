9ebc26d9babf620b94c4b6f60a43d13e0612055d
Illia Kuzmenko <illia.kuzmenko@icloud.com>
2023-01-16 17:13:33 +0200

    add GetInterviewTemplatesByCompanyUseCase

---
+++ src/module/interview-template/core/useCase/GetInterviewTemplatesByCompanyUseCase.ts
@@ -0,0 +1,20 @@
+import {CommandUseCase} from "../../../../lib/model/CommandUseCase";
+import {NotFoundException} from "../../../../lib/model/app-exception/NotFoundException";
+import {InterviewTemplateRepository} from "../port/InterviewTemplateRepository";
+import {InterviewTemplateEntity} from "../../../../db/postgre/entities/InterviewTemplateEntity";
+
+export class GetInterviewTemplatesByCompanyUseCase implements CommandUseCase<string, Promise<InterviewTemplateEntity[]>> {
+    constructor(
+        private  interviewTemplateRepository: InterviewTemplateRepository
+    ) {}
+
+    async execute(id: string): Promise<InterviewTemplateEntity[]> {
+        const interviewTemplate = await this.interviewTemplateRepository.getInterviewTemplatesByCompany(id);
+
+        if (interviewTemplate == null) {
+            throw new NotFoundException(`InterviewTemplate with id: ${id} was not found!`)
+        }
+
+        return interviewTemplate;
+    }
+}
