31a78c685a3ae94278a22a76608c002d9de7c942
Illia Kuzmenko <illia.kuzmenko@icloud.com>
2023-01-16 17:14:43 +0200

    add PostgresInterviewTemplateRepository

---
+++ src/module/interview-template/repository/PostgresInterviewTemplateRepository.ts
@@ -0,0 +1,41 @@
+import {InterviewTemplateRepository} from "../core/port/InterviewTemplateRepository";
+import {InterviewTemplate} from "../core/model/InterviewTemplate";
+import {CreateInterviewTemplate} from "../core/model/CreateInterviewTemplate";
+import interviewTemplateRepository from "../../../db/postgre/repositories/interviewTemplateRepository";
+import companyRepository from "../../../db/postgre/repositories/companyRepository";
+import {NotFoundException} from "../../../lib/model/app-exception/NotFoundException";
+import {InterviewTemplateEntity} from "../../../db/postgre/entities/InterviewTemplateEntity";
+
+export class PostgresInterviewTemplateRepository implements InterviewTemplateRepository {
+
+    async createInterviewTemplate(interviewTemplate: CreateInterviewTemplate): Promise<InterviewTemplate> {
+        const company = await companyRepository.findOneBy({id: interviewTemplate.companyId })
+
+        if (company == null) {
+            throw new NotFoundException("Company is not found!")
+        }
+
+        const interviewTemplateEntity = interviewTemplateRepository.create({
+            name: interviewTemplate.name,
+            company: company,
+        });
+
+        await interviewTemplateRepository.save(interviewTemplateEntity)
+        console.log('INTERVIEW-TEMPLATE WAS CREATED', interviewTemplateEntity);
+
+        return {
+            id: interviewTemplateEntity.id,
+            name: interviewTemplateEntity.name,
+        }
+    }
+
+    async getInterviewTemplatesByCompany(id: string): Promise<InterviewTemplateEntity[]> {
+        return await interviewTemplateRepository.find({where:{id: id}});
+    }
+
+    async isInterviewTemplate(interviewTemplateName: string): Promise<boolean> {
+        const interviewTemplate = await interviewTemplateRepository.findOneBy({name: interviewTemplateName});
+        return !!interviewTemplate;
+    }
+
+}
