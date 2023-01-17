d2bc6c09e9ccc9616563b4167502dceff2983986
Illia Kuzmenko <illia.kuzmenko@icloud.com>
2023-01-17 18:11:41 +0200

    promise fixed

--- src/module/interview-template/core/useCase/UpdateInterviewTemplateUseCase.ts
+++ src/module/interview-template/core/useCase/UpdateInterviewTemplateUseCase.ts
@@ -2,13 +2,13 @@ import {CommandUseCase} from "../../../../lib/model/CommandUseCase";
 import {InterviewTemplateRepository} from "../port/InterviewTemplateRepository";
 import {InterviewTemplate} from "../model/InterviewTemplate";
 
-export class UpdateInterviewTemplateUseCase implements CommandUseCase<InterviewTemplate, Promise<InterviewTemplate>> {
+export class UpdateInterviewTemplateUseCase implements CommandUseCase<InterviewTemplate> {
 
     constructor(
         private  interviewTemplateRepository: InterviewTemplateRepository
     ) {}
 
-    async execute(data: InterviewTemplate): Promise<InterviewTemplate> {
+    async execute(data: InterviewTemplate): Promise<void> {
         return await this.interviewTemplateRepository.updateInterviewTemplate(data);
     }
 }
