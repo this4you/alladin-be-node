f5871cb67e7effd3aa527997b45c4f6bd7645eec
Illia Kuzmenko <illia.kuzmenko@icloud.com>
2023-01-21 14:43:58 +0200

    add useCases interview-template-step

---
+++ src/module/interview-template-step/core/useCase/GetInterviewTemplateStepByInterviewTemplateUseCase.ts
@@ -0,0 +1,13 @@
+import {CommandUseCase} from "../../../../lib/model/CommandUseCase";
+import {InterviewTemplateStepRepository} from "../port/InterviewTemplateStepRepository";
+import {InterviewTemplateStep} from "../model/InterviewTemplateStep";
+
+export class GetInterviewTemplateStepByInterviewTemplateUseCase implements CommandUseCase<string, Promise<InterviewTemplateStep[]>> {
+    constructor(
+        private  interviewTemplateStepRepository: InterviewTemplateStepRepository
+    ) {}
+
+    async execute(interviewTemplateId: string): Promise<InterviewTemplateStep[]> {
+        return await this.interviewTemplateStepRepository.getInterviewTemplateStepsByInterviewTemplate(interviewTemplateId);
+    }
+}
