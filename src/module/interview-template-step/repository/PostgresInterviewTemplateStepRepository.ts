8e8abf106354c68a9cbd385ae91cc434a91be370
Illia Kuzmenko <illia.kuzmenko@icloud.com>
2023-01-21 14:45:18 +0200

    connect interview-template-step to db

---
+++ src/module/interview-template-step/repository/PostgresInterviewTemplateStepRepository.ts
@@ -0,0 +1,50 @@
+import {InterviewTemplateStepRepository} from "../core/port/InterviewTemplateStepRepository";
+import {InterviewTemplateStep} from "../core/model/InterviewTemplateStep";
+import {CreateInterviewTemplateStep} from "../core/model/CreateInterviewTemplateStep";
+import {NotFoundException} from "../../../lib/model/app-exception/NotFoundException";
+
+import interviewTemplateRepository from "../../../db/postgre/repositories/interviewTemplateRepository";
+import interviewTemplateStepRepository from "../../../db/postgre/repositories/interviewTemplateStepRepository";
+
+export class PostgresInterviewTemplateStepRepository implements InterviewTemplateStepRepository {
+    async createInterviewTemplateStep(interviewTemplateStep: CreateInterviewTemplateStep): Promise<InterviewTemplateStep> {
+        const interviewTemplate = await interviewTemplateRepository.findOneBy({id: interviewTemplateStep.interviewTemplateId})
+
+        if (interviewTemplate == null) {
+            throw new NotFoundException("InterviewTemplate is not found!")
+        }
+
+        const interviewTemplateStepEntity = interviewTemplateStepRepository.create({
+            name: interviewTemplateStep.name,
+            interviewTemplate: interviewTemplate,
+        });
+
+        await interviewTemplateStepRepository.save(interviewTemplateStepEntity);
+        console.log('INTERVIEW-TEMPLATE WAS CREATED', interviewTemplateStepEntity);
+
+        return {
+            id: interviewTemplateStepEntity.id,
+            name: interviewTemplateStepEntity.name,
+        }
+
+    }
+
+    async getInterviewTemplateStepsByInterviewTemplate(interviewTemplateId: string): Promise<InterviewTemplateStep[]> {
+        return await interviewTemplateStepRepository.findBy({interviewTemplate: {id: interviewTemplateId}});
+    }
+
+    async deleteInterviewTemplateStep(id: string): Promise<void> {
+        await interviewTemplateStepRepository.delete(id);
+    }
+
+    async updateInterviewTemplateStep(interviewTemplateStep: InterviewTemplateStep): Promise<InterviewTemplateStep> {
+        return await interviewTemplateStepRepository.save({
+            id: interviewTemplateStep.id,
+            name: interviewTemplateStep.name,
+        });
+    }
+
+    async isInterviewTemplateStepByName(name: string): Promise<boolean> {
+        return !!await interviewTemplateStepRepository.findOneBy({name: name});
+    }
+}
