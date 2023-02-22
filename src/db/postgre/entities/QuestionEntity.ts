1ec8b62b9a8249e9e3c5f76193a54c596e8c6f23
Illia Kuzmenko <illia.kuzmenko@icloud.com>
2023-02-22 17:07:36 +0200

    fix entity

--- src/db/postgre/entities/QuestionEntity.ts
+++ src/db/postgre/entities/QuestionEntity.ts
@@ -1,10 +1,16 @@
-import {Column, Entity} from "typeorm";
+import {Column, Entity, JoinColumn, ManyToOne} from "typeorm";
 import {BaseEntity} from "@db/postgre/entities/BaseEntity";
+import {QuestionCategoryEntity} from "@db/postgre/entities/QuestionCategoryEntity";
 
 @Entity({name: 'Question'})
 export class QuestionEntity extends BaseEntity {
     @Column()
-    text: string
-    @Column()
-    questionCategoryId: string
+    text: string;
+
+    @Column({ nullable: false })
+    questionCategoryId: string;
+
+    @ManyToOne(() => QuestionCategoryEntity)
+    @JoinColumn()
+    questionCategory: QuestionCategoryEntity;
 }
