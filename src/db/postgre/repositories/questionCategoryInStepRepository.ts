cc8e5b15a0baf77892fe9d5ed5ee49a1c07cb6e7
Illia Kuzmenko <illia.kuzmenko@icloud.com>
2023-02-23 12:12:04 +0200

    change dir

--- src/db/postgre/repositories/questionCategoryInStepRepository.ts
+++
@@ -1,6 +0,0 @@
-import { AppDataSource } from '../data-source';
-import {QuestionCategoryInStepEntity} from "../entities/QuestionCategoryInStepEntity";
-
-const questionCategoryInStepRepository = AppDataSource.getRepository(QuestionCategoryInStepEntity);
-
-export default questionCategoryInStepRepository;
--- src/module/interview-template/question-category-in-step/core/model/QuestionCategoryInStep.ts
+++
@@ -1,3 +0,0 @@
-export type QuestionCategoryInStep = {
-    id: string;
-}
