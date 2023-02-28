78e5c2be568fe9aded500a90ab1d315e67fe5c00
Illia Kuzmenko <illia.kuzmenko@icloud.com>
2023-02-28 13:24:25 +0200

    delete interface, use inline QuestionWithStep obj

--- src/module/interview-template/step-category/core/model/StepCategory.ts
+++ src/module/interview-template/step-category/core/model/StepCategory.ts
@@ -1,12 +1,8 @@
 import {Question} from "@module/interview-template/question/core/model/Question";
 
-interface NewQuestion extends Question {
-    position: number;
-}
-
 export type StepCategory = {
     stepCategoryId: string,
     categoryName: string,
     position: number,
-    questions: NewQuestion[]
+    questions: (Question&{position: number})[]
 }
